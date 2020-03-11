205b4f1bfb7ad661478eb8a5af3301e2ca411083
gmolki <gerardmo98@gmail.com>
2020-03-11 01:32:23 +0100

    "Receives response from server when login"

--- src/clientConnection.js
+++ src/clientConnection.js
@@ -14,13 +14,50 @@ socket.onerror = function(error) {
 
 socket.onmessage = function(msg) {
   console.log("Received:\t" + msg.data);
+
+  var recovered_msg = JSON.parse(msg.data);
+  console.log(recovered_msg);
+
+  switch (recovered_msg.code) {
+    case codes.fromServer.loginChecked:
+      onLoginChecked(recovered_msg.data);
+      break;
+
+    default:
+      console.log("Server could not recognize the code:\t", recovered_msg.code);
+  }
 };
 
 function sendMessage(data) {
   socket.send(JSON.stringify(data));
 }
 
+function onLoginChecked(loginResponse) {
+  switch (loginResponse.code) {
+    case codes.fromServer.userAlreadyConnected:
+      console.log("That username is being used right now bro!");
+      break;
+    case codes.fromServer.newUserRegistered:
+      console.log("Welcome to moviEarth! ^o^");
+      break;
+    case codes.fromServer.correctLogin:
+      console.log("Welcome back to moviEarth! ^o^");
+      break;
+    default:
+      console.log("Unexpected error caused in the server! :(");
+      break;
+  }
+}
+
 const codes = {
-  newConnection: 0,
-  login: 1
+  toServer: {
+    newConnection: 0,
+    login: 1
+  },
+  fromServer: {
+    loginChecked: 0,
+    userAlreadyConnected: 1,
+    newUserRegistered: 2,
+    correctLogin: 3
+  }
 };
--- src/login.js
+++ src/login.js
@@ -23,7 +23,7 @@ window.addEventListener("load", function() {
     var rememberMe = signIn["rememberMe"].checked;
 
     var message = {
-      code: codes.login,
+      code: codes.toServer.login,
       username: username,
       rememberMe: rememberMe
     };
